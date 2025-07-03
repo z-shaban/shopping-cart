a90bc9cafdf847ec7b10238d471b2a1241ac3e9c
shaban <146794146+sarzhh@users.noreply.github.com>
2025-07-03 23:04:55 +0000

    add styles to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,27 @@ function App() {
 
   return (
     <>
-      <header className="flex gap-4 bg-amber-500">
+      <div className="min-h-screen flex flex-col">
+
+      <header className="flex justify-between bg-black text-white p-8">
         <h1>glow cosmetics</h1>
-       <div>
-        <Link to='home'>Home</Link>
-        <Link to='products'>Products</Link>
-        <Link to='cart'>Cart</Link>
+       <div className="flex gap-4">
+        <Link to='home' className="hover:underline">Home</Link>
+        <Link to='products' className="hover:underline">Products</Link>
+        <Link to='cart' className="hover:underline">Cart</Link>
        </div>
       </header>
 
-      <main>
+      <main className="flex-1 flex flex-col justify-center items-center">
        <Outlet/>
       </main>
-      <footer>
+      <footer className="flex justify-center">
         <p>@2025</p>
       </footer>
 
+
+      </div>
+      
     </>
   )
 }
