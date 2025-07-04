4b7a244ed15860b5759d471e8c3853d26dcd7326
shaban <146794146+sarzhh@users.noreply.github.com>
2025-07-04 23:11:22 +0000

    add logic for quantity of items and fetch data from dummy json api

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react"
+
+
+
 export function ProductCard({card}){
+   
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrement= ()=>{
+    setQuantity(quantity + 1)
+  }
+
+  const handleDecrement = ()=>{
+    if(quantity > 1){
+      setQuantity(quantity - 1)
+    }
+  }
+ 
+ 
     return <>
     <div className="flex flex-col gap-8 border-2 border-black p-4  rounded">
-      <div>{card.name}</div>
-      <div>{card.price}</div>
+      <div><img src={card.image} alt="" /></div>
+      <div>{card.title}</div>
+      <div>${card.price * quantity}</div>
       <div>
         <label htmlFor="">Quantity: </label>
        <input
        type="text"
-       value={1}
+       value={quantity}
+       onChange={(e) => setQuantity(e.target.value)}
        />
         <br></br>
-        <button className="border-2 border-black bg-black text-white p-1 rounded hover:bg-white hover:text-black mr-4 ">+</button>
-        <button className="border-2 border-black bg-black text-white p-1 rounded hover:bg-white hover:text-black ">-</button>
+        <button onClick={handleIncrement} className="border-2 border-black bg-black text-white p-1 rounded hover:bg-white hover:text-black mr-4 ">+</button>
+        <button onClick={handleDecrement} className="border-2 border-black bg-black text-white p-1 rounded hover:bg-white hover:text-black ">-</button>
       </div>
       <button className="border-2 border-black bg-black text-white p-1 rounded hover:bg-white hover:text-black ">
         ADD TO CART
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,21 +1,49 @@
 import { ProductCard } from "./productCard"
+import { useState, useEffect } from "react"
+
 
 export function Products(){
+const [products, setProducts] = useState(null);
+const [error, setError] = useState(null)
+const [loading, setLoading] = useState(true)
 
+useEffect(()=>{
+    fetch('https://dummyjson.com/products/category/skin-care')
+    .then((response) => {
+      if (response.status >= 400) {
+        throw new Error("server error");
+      }
+      return response.json();
+    })
+    .then(data => {
+        const myProducts = data.products.map(product=> ({
+            id: product.id,
+            title: product.title,
+            price : product.price,
+            image: product.images[0]
+        }))
+        setProducts(myProducts)
+    })
+    .catch((error)=> setError(error))
+    .finally(()=> setLoading(false))
+},[])
 
-const Products = [
-    {name: 'karamel set' , price: '$200'},
-    {name: 'acne set' , price:'$150'},
-    {name:'sunburn kit' , price: '$100'}
+if (loading) return <p>Loading...</p>;
+if (error) return <p>A network error was encountered</p>;
 
-]
+/*const Products = [
+    {name: 'karamel set' , price: 200},
+    {name: 'acne set' , price:150},
+    {name:'sunburn kit' , price: 100}
+
+]*/
 
 return(
     <>
     <div className="flex flex-col gap-4 items-center min-h-screen pt-4">
     <h1>PRODUCTS</h1>
     <div className="flex gap-8">
-          {Products.map((product)=>(<ProductCard card={product}/>))}
+          {products.map((product)=>(<ProductCard key={product.id} card={product}/>))}
     </div>
    
     </div>
